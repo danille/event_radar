3c0a3fbdbdea0bbad9e59a9a9ec1929d8f7a2ec9
Danylo Lebediev <danilleock@gmail.com>
2017-10-26 16:25:33 +0200

    RSS feeds are parsed + content is saved in JSON files.

--- src/collect_data.py
+++ src/collect_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import feedparser as fp
 import json
 import os
+import re
 from utils import EXTERNAL_DATA_DIR, RAW_DATA_DIR, TODAY, make_dir
 
 
@@ -15,9 +16,15 @@ def get_feeds_list(path_to_file: str) -> list:
     return rss
 
 
-def parse_feed(feed_url: str):
+def parse_feed(feed_url: str) -> list:
     d = fp.parse(feed_url)
-    return d
+    entries = []
+    for entry in d.entries:
+        entry_dict = {'title': entry['title'],
+                      'summary': entry['summary'],
+                      'link': entry['link']}
+        entries.append(entry_dict)
+    return entries
 
 
 def get_feed_title_from_url(feed_url: str) -> str:
@@ -26,8 +33,10 @@ def get_feed_title_from_url(feed_url: str) -> str:
     :param feed_url: RSS feed's URL, which title should be extracted
     :return: title of RSS feed
     """
+    feed_title = str(re.search(r"//[w\d.]*([\S][^/]+)", feed_url).group(1))
+    return feed_title
 
-    
+
 def write_list_to_JSON(list_of_objects, path_to_json: str) -> None:
     with open(path_to_json, 'w') as json_file:
         json.dump(list_of_objects, json_file)
@@ -43,7 +52,7 @@ def collect_data_from_RSS_feeds():
 
         make_dir(os.path.join(RAW_DATA_DIR, TODAY))
 
-        feed_title = feed.feed.title
+        feed_title = get_feed_title_from_url(rss_url)
         json_file_path = os.path.join(RAW_DATA_DIR, TODAY, feed_title)
 
         write_list_to_JSON(feeds, json_file_path)
